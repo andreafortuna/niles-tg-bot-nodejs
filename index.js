19699e43835f722ba159e29de32b1e09d97b8cfa
andrea.fortuna <andrea.fortuna@FortunaA-OSX-SUB.station>
2019-12-27 18:18:48 +0100

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+const Telegraf = require('telegraf');
+const bot = new Telegraf("498078166:AAEnVj8LKnbmhFJM9brKVUb5E7KXfNMhjBU");
+
+bot.command('image', (ctx) => ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' }))
+
+bot.on('text', ({ replyWithHTML }) => {
+    console.log('Incoming');
+    return replyWithHTML('<b>Hello</b>')
+})
+
+bot.telegram.setWebhook('https://8129c606.ngrok.io/webhook')
+
+bot.startWebhook('/webhook', null, 5000)
+
