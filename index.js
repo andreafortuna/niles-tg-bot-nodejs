bde08a22913e386c0d298647c55a25269b1cf5f0
andrea.fortuna <andrea.fortuna@FortunaA-OSX-SUB.station>
2019-12-28 11:11:57 +0100

    heroku

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Telegraf = require('telegraf');
 const bot = new Telegraf("498078166:AAEnVj8LKnbmhFJM9brKVUb5E7KXfNMhjBU");
 
 bot.command('image', (ctx) => ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' }))
+bot.command('test', (ctx) => ctx.replyWithHTML("TEST!"))
+
+
 
 bot.on('text', ({ replyWithHTML }) => {
     console.log('Incoming');
