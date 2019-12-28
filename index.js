a7c5fa83008ec7dfeceacc43677f76013dfe4fe5
andrea.fortuna <andrea.fortuna@FortunaA-OSX-SUB.station>
2019-12-28 21:39:08 +0100

    index

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const bot = new Telegraf("498078166:AAEnVj8LKnbmhFJM9brKVUb5E7KXfNMhjBU");
 bot.command('image', (ctx) => ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' }))
 bot.command('test', (ctx) => ctx.replyWithHTML("TEST!"))
 
-
-
 bot.on('text', ({ replyWithHTML }) => {
     console.log('Incoming');
     return replyWithHTML('<b>Ciao!</b>')
 })
-const PORT = process.env.PORT || 3000;
-//const URL = "https://agile-shelf-44940.herokuapp.com/webhook"
+const PORT = process.env.PORT 
 const URL = process.env.BASE_URL + "webhook"
 console.log ("Registering webhook:" + URL )
 bot.telegram.setWebhook(URL)
