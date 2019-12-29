2705c8b9fbeb4b3fa22663502e835e7bb3f0df10
andrea.fortuna <andrea.fortuna@FortunaA-OSX-SUB.station>
2019-12-29 09:22:48 +0100

    code cleaning

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.TGTOKEN);
 
+// Some sample commands...
+bot.start((ctx) => {
+    ctx.reply(`Hello ${ctx.state.role}`)
+})
+bot.help((ctx) => {
+    ctx.reply("HELP MESSAGE...")
+})
+
+
+bot.command('test', (ctx) => {
+    ctx.reply(`Hello ${ctx.state.role}, this is a TEST!`)
+})
 
 bot.command('image', ({ replyWithPhoto }) => {
     console.log('IMAGE');
     return replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' })
 })
 
-bot.command('test', ({ replyWithHTML }) => {
-    console.log('TEST');
-    return replyWithHTML('<b>TEST!</b>')
+
+// Default reply
+bot.on('text', (ctx) => {
+    ctx.reply(`Hello!`)
 })
-bot.on('text', ({ replyWithHTML }) => {
-    console.log('Incoming');
-    return replyWithHTML('<b>Ciao!</b>')
-})
+
+
 const PORT = process.env.PORT 
 const URL = process.env.BASE_URL + "webhook"
 console.log ("Registering webhook:" + URL )
 bot.telegram.setWebhook(URL)
-
 bot.startWebhook('/webhook', null, PORT)
 
