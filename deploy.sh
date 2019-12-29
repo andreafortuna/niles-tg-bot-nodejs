9b2fc53726f6315c602a077756bccec49da89896
andrea.fortuna <andrea.fortuna@FortunaA-OSX-SUB.station>
2019-12-29 12:09:42 +0100

    docker

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 heroku create
 heroku config:set BASE_URL=$(heroku info -s | grep web_url | cut -d= -f2)
 heroku config:set TGTOKEN=$1
-heroku stack:set container
+#heroku stack:set container
 git push heroku master
 heroku logs --tail
 heroku apps:destroy
