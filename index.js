71fa6ff3479efe43d047e5cd9f771e3bf7498d31
andrea.fortuna <andrea.fortuna@FortunaA-OSX-SUB.station>
2019-12-30 13:24:18 +0100

    names

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Telegraf(process.env.TGTOKEN);
 
 // Some sample commands...
 bot.start((ctx) => {
-    ctx.reply(`Hello ${ctx.message.from.first_name} ${ctx.message.from.second_name}`)
+    ctx.reply(`Hello ${ctx.message.from.first_name} ${ctx.message.from.last_name}`)
 })
 bot.help((ctx) => {
     ctx.reply("HELP MESSAGE...")
@@ -11,7 +11,7 @@ bot.help((ctx) => {
 
 
 bot.command('test', (ctx) => {
-    ctx.reply(`Hello ${ctx.message.from.first_name} ${ctx.message.from.second_name}, this is a TEST!`)
+    ctx.reply(`Hello ${ctx.message.from.first_name} ${ctx.message.from.last_name}, this is a TEST!`)
 })
 
 bot.command('image', ({ replyWithPhoto }) => {
