502b3bf87d10e951ac6f3b9b7ab4f14854bd9a06
andrea.fortuna <andrea.fortuna@fortunaa-osx-sub.local>
2020-01-02 10:24:35 +0100

    readme

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 heroku create
 heroku config:set BASE_URL=$(heroku info -s | grep web_url | cut -d= -f2)
 heroku config:set TGTOKEN=$1
+# Uncomment the next line to deploy as container
 #heroku stack:set container
 git push heroku master
 heroku logs --tail
